330af7c0f1ad396bf329211b77df19bc4b90dc21
palakkaushik <kaushik.palak05@gmail.com>
2024-11-25 23:57:22 +0530

    backend working done

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const { PositionsModel } = require("./model/PositionsModel");
 const { OrdersModel } = require("./model/OrdersModel");
 
 const PORT = process.env.PORT || 3002;
-const uri = process.env.MONGO_URL;
+const uri = process.env.MONGO_URI;
 
 const app = express();
 
